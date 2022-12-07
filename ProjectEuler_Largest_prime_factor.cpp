705ffe53dda694bfcea789c6557a95c6b81c8940
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-07 15:07:35 +0800

    Update and rename largestprimefactor.cpp to ProjectEuler_Largest_prime_factor.cpp

--- ProjectEuler_Largest_prime_factor.cpp
+++ ProjectEuler_Largest_prime_factor.cpp
@@ -1,9 +1,9 @@
-
+//This does work with the sample number but it does not with the actual question.
 #include <vector>
 #include <bits/stdc++.h>
 
 using namespace std;
-bool is_prime(int n)
+bool is_prime(int n)//Finds out whether if it is a prime number or not
 {
   if (n < 2) return false;
   for (int i = 2; i <= sqrt(n); i++)
@@ -17,14 +17,13 @@ bool is_prime(int n)
 }
 int main() {
   vector<int>prime = {};
-  vector<int>ans = {};
   int count =0;
-   for (long long i =2;i<13195;i++){
+   for (long long i =2;i<13195;i++){//finds factors for the number given
     if((13195)%i==0){
         if (is_prime(i)){
           prime.push_back(i);
         }
       }
     }
-  cout<<*max_element(prime.begin(), prime.end());
+  cout<<*max_element(prime.begin(), prime.end());//gets the largest prime factor in the vector
   }
