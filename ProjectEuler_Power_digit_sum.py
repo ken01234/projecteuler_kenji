431bb98a5c8cfc1f45b37bccb6c5162b170e9382
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-07 16:24:08 +0800

    Add files via upload

---
+++ ProjectEuler_Power_digit_sum.py
@@ -0,0 +1,10 @@
+power = 2**1000
+s1 =str(power) #the numbers into string
+sum=0
+numbers=[]
+for i in range (len(s1)):
+  numbers.append(s1[i]) #the numbers are added into a list and split into individual strings 
+for n2 in numbers:
+  n3 = int(n2) #the numbers are then returned back into integers and added into the sum 
+  sum+=n3
+print(sum)
