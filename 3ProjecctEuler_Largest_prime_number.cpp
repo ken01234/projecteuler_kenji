58c64746c8d0362cfbbaae319ca27ba11da2778a
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-12 03:10:20 +0800

    Update 3ProjecctEuler_Largest_prime_number.cpp

--- 3ProjecctEuler_Largest_prime_number.cpp
+++ 3ProjecctEuler_Largest_prime_number.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-long long primefactor(long long p){
+long long primefactor(long long p){// p is just a variable
     int i =2; 
-  while((i*i)<p){
-    while(p%i==0){
+  while((i*i)<p){// the sqyare of the factor should not be more than the number given so it drastically reduces the range of numbers that needs to be checked
+    while(p%i==0){// This part will attempt to see if it can divide by i, if it can then it will shorten the number. This helps to make it faster.
       p/=i;
     }
     i++;
