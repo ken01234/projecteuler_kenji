9808ed5a334a3e953f8ad5834f9759264397b697
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-12 10:41:08 +0800

    Add files via upload

---
+++ 5ProjectEuler_Smallest_multiple.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+int main(){
+for(long long i =1;i<1000000000;i++){
+  if (i%16 == 0){
+    if(i%9== 0){
+      if(i%5 == 0){
+        if(i%7 == 0){
+          if(i%17 ==0){
+            if(i%19 == 0){
+              if(i%11==0){
+                if(i%13==0){
+              cout<<i<<endl;
+          break;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+    }
+  }
+  }
